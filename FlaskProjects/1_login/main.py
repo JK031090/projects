b8f4d25c8f75a56752272c032e367615c487ae07
JK <jiteshkrishna31090@gmail.com>
2020-04-22 01:53:42 +0530

    master

--- FlaskProjects/1_login/main.py
+++ FlaskProjects/1_login/main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def test():
+def home():
     return render_template('index.html')
 
+@app.route("/signup" , methods=['GET','POST'])
+def users():
+    if request.method == 'POST':
+        req = request.form
+        print(req)
+
+        return redirect(request.url)
+
+
+    return render_template('users.html')
 if __name__ == '__main__':
     app.run(debug = True)
